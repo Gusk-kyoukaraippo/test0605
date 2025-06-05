7f536436661517469756f5c36594c1de151b3207
Gusk-kyoukaraippo <kyoukaraippo@gmail.com>
2025-06-06 04:25:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def load_llama_index_from_gcs():
         st.success(f"{download_count} 個のインデックスファイルをGCSからダウンロードしました。")
 
         # 埋め込みモデルを設定
-        Settings.embed_model = GoogleGenAIEmbedding(model_name="gemini-embedding-exp-03-07", embed_hparams={"output_dimensionality": 1536})
+        Settings.embed_model = GoogleGenAIEmbedding(model_name="gemini-embedding-001", embed_hparams={"output_dimensionality": 1536})
 
         # 埋め込みモデルが機能するかテスト
         try:
